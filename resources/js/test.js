884db57c194a3b9da4446637e9438c767e0e5e43
Amey Kamat <ask5332@gmail.com>
2016-09-11 13:59:44 +0530

    Initial UI commit

---
+++ resources/js/test.js
@@ -0,0 +1,3 @@
+$.ajax({url:"http://www.thomas-bayer.com/sqlrest/CUSTOMER/18/", success: function(result){
+	console.log(result);
+}})
---
+++ resources/js/util.js
@@ -0,0 +1,9 @@
+var source = "http://192.168.43.184:8080"
+var GET_LAYOUT_URL = source + "/REST_MAVEN/webresources/mohit/getLayoutInfo";
+var GET_DESK_URL = source + "/REST_MAVEN/webresources/mohit/getDeskInfo";
+var GET_EMPLOYEE_URL = "";
+// Defines all quantities wrt gridSize
+var chairDef = {
+	"paddingRatio" : 0.0625,
+	"borderRadiusRatio" : 0.125
+}
